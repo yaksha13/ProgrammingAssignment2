e3fdc0117544d2a09db1675dc28fd06541bb01de
Akshay Rao <yaksha13@users.noreply.github.com>
2015-03-22 22:34:17 +0530

    Complete cacheSolve function and added comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,7 @@
 
 
 ## makeCacheMatrix returns a list of 4 functions on a provided Matrix "x"
+
 #  inverse_cache - represents a cache for the inverse of Matrix "x"
 #  get -  Returns the contents of the Matrix "x"
 #  set(y) - Assigns "x" the values in Matrix "y" and clears contents in "inverse_cache"
@@ -30,9 +31,33 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve evaluates the inverse for a Matrix object "x" of the type generated
+## by "makeCacheMatrix" function.
+
+## It checks if "x" has a computed inverse in its cache from the "getInverse" function.
+## If TRUE, it returns the cached inverse.
+## If NOT TRUE, it calculates the inverse using the "solve" function and stores 
+## the inverse in the cache using the "setInverse" function and returns the inverse
+
+# inverse - stores the inverse of the Matrix "x"
+# tempMatrix - temporarily stores the value of Matrix "x" for computing the inverse
 
 cacheSolve <- function(x, ...) {
+        inverse <- x$getInverse
         
-        ## Return a matrix that is the inverse of 'x'
+        # to check if inverse has been cached
+        if (!is.null(inverse)){
+                message("getting cached inverse")
+                
+                # return the cached inverse
+                return(inverse)
+        }
+        
+        tempMatrix <- x$get
+        # solve(x) returns the inverse matrix for "x"
+        inverse <- solve(tempMatrix, ...)
+        # set the computed inverse
+        x$setInverse(inverse)
+        # return the inverse
+        inverse
 }
